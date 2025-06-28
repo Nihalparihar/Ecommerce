071f4b1907cd5a45860003cec763a510504e7b58
Nihal Singh Parihar <nihalsingh.10821737@ltimindtree.com>
2025-06-28 15:44:10 +0530

    4 Commit

--- src/test/java/demoTest/StandAloneTest.java
+++ src/test/java/demoTest/StandAloneTest.java
@@ -2,17 +2,21 @@ package demoTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class StandAloneTest {
-    public static void main(String[] args) {
 
-        WebDriver driver=new ChromeDriver();
+    @Test
+    public void ecomTest()
+    {
+        WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         System.out.println("==========STARTED=========");
         driver.get("https://rahulshettyacademy.com/client");
     }
+
 }
